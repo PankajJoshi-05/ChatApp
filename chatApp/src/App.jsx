633058d59abf2d2d481fa29226a09902000943f3
unknown <joshipankajjoshi9456@gmail.com>
2024-04-28 01:24:33 +0530

    initialize a repo

---
+++ chatApp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Home from './pages/Home';
+function App() {
+
+
+  return (
+    // <Register/>
+    // <Login/>
+    <Home/>
+    
+  )
+}
+
+export default App
---
+++ chatApp/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './style.scss';
+import Add from "../img/addAvatar.png";
+const Login=()=>{
+    return(
+        <div className="formContainer">
+            <div className="formWrapper">
+                <span className="logo">Chat App</span>
+                <span className="title">Login</span>
+                <form >
+                    <input type="email" placeholder="email"/>
+                    <input type="password"placeholder="password" />
+                    <button>Sign in</button>
+                </form>
+                <p>Don't have an account</p>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
