1482e85813c21e7991f276f9d75f2eea1389f0ce
unknown <joshipankajjoshi9456@gmail.com>
2024-04-29 23:02:49 +0530

    authentication using firebase

--- chatApp/src/firebase.jsx
+++ chatApp/src/firebase.jsx
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
-
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyA2lrJenzii83ezxPrLOv8zdUY1KjsTsZU",
   authDomain: "chat-8ff6e.firebaseapp.com",
@@ -14,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth=getAuth();
+export const storage = getStorage();
--- chatApp/src/pages/Register.jsx
+++ chatApp/src/pages/Register.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Add from "../img/addAvatar.png";
-const Register=()=>{
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth,storage} from "../firebase";
+import {ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
-    const handleSunmit=(e)=>{
+const Register=()=>{
+    const [err,setErr]=useState(false);
+    const handleSubmit=async(e)=>{
         e.preventDefault();
-        console.log(e.target[3]);
         const displayName=e.target[0].value;
         const email=e.target[1].value;
         const password=e.target[2].value;
         const file=e.target[3].files[0];
+
+try{
+const res= await createUserWithEmailAndPassword(auth, email, password)
+console.log(res)
+const storageRef = ref(storage, displayName);
+
+const uploadTask = uploadBytesResumable(storageRef, file);
+
+uploadTask.on(
+
+  (error) => {
+     setErr(true);
+  }, 
+  () => {
+    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+      await updateProfile(res.user,{
+        displayName,
+        photoURL:downloadURL,
+      });
+    });
+  }
+);
+
+}catch(err){
+   setErr(true);
+}
     }
 
     return(
@@ -16,7 +45,7 @@ const Register=()=>{
             <div className="formWrapper">
                 <span className="logo">Chat App</span>
                 <span className="title">Register</span>
-                <form onSubmit={handleSunmit}>
+                <form onSubmit={handleSubmit}>
                     <input type="text"placeholder="display name" />
                     <input type="email" placeholder="email"/>
                     <input type="password"placeholder="password" />
@@ -26,6 +55,7 @@ const Register=()=>{
                         <span>Add an avatar</span>
                     </label>
                     <button>Sign Up</button>
+                    {err && <span>Something went Wrong</span>}
                 </form>
                 <p>Already have account</p>
             </div>
